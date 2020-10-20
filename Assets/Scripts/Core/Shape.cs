f97b3b026556db4f025690cc6abab68175ba4589
Fatih <fatihaltuntas44@gmail.com>
2020-10-20 21:05:08 +0300

    Board, Shape, GameController and Spawner classes created

--- Assets/Scripts/Core/Shape.cs
+++ Assets/Scripts/Core/Shape.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool m_isRotate = true;
+
+    void Move(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+    
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+    
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+    
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void RotateLeft()
+    {
+        if (m_isRotate)
+        {
+            transform.Rotate(new Vector3(0, 0, 90));
+        }
+    }
+    
+    public void RotateRight()
+    {
+        if (m_isRotate)
+        {
+            transform.Rotate(new Vector3(0, 0, -90));
+        }
+    }
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Board m_gameBoard;
+    private Spawner m_spawner;
+    
+    void Start()
+    {
+        //m_gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+        //m_spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        m_gameBoard = GameObject.FindObjectOfType<Board>();
+        m_spawner = GameObject.FindObjectOfType<Spawner>();
+
+        if (m_spawner)
+        {                                //Vector3Int.RoundToInt(m_spawner.transform.position);
+            m_spawner.transform.position = Vectorf.Round(m_spawner.transform.position);
+        }
+
+        if (!m_gameBoard)
+        {
+            Debug.LogWarning("WARNING: There is no game board defined!");
+        }
+        
+        if (!m_spawner)
+        {
+            Debug.LogWarning("WARNING: There is no spawner defined!");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
