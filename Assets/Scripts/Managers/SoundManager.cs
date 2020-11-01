2ed29c786b32700e34117675af45b6e55567369a
Fatih <fatihaltuntas44@gmail.com>
2020-11-01 15:30:33 +0300

    setup SoundManager class which will help playback sound effects and music

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public bool m_musicEnabled = true;
+    public bool m_fxEnabled = true;
+
+    [Range(0,1)]
+    public float m_musicVolume = 1.0f;
+    
+    [Range(0,1)]
+    public float m_fxVolume = 1.0f;
+
+    public AudioClip m_clearRowSound;
+    public AudioClip m_moveSound;
+    public AudioClip m_dropSound;
+    public AudioClip m_gameOverSound;
+    public AudioClip m_backgroundSound;
+    
+}
