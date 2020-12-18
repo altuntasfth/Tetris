11e03e293ee63466d4c170ecec67263dfab8ef55
Fatih <fatihaltuntas44@gmail.com>
2020-12-18 12:50:46 +0300

    setup an AudioSource for our BackgroundMusic audio. - configure the SoundManager to control music playback.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SoundManager : MonoBehaviour
 {
@@ -17,6 +19,59 @@ public class SoundManager : MonoBehaviour
     public AudioClip m_moveSound;
     public AudioClip m_dropSound;
     public AudioClip m_gameOverSound;
-    public AudioClip m_backgroundSound;
-    
+    //public AudioClip m_backgroundSound;
+
+    public AudioClip[] m_musicClips;
+    private AudioClip m_randomMusicClip;
+
+    public AudioSource m_musicSource;
+
+    private void Start()
+    {
+        //m_randomMusicClip = GetRandomClip(m_musicClips);
+        //PlayBackgroundMusic(m_randomMusicClip);
+        
+        PlayBackgroundMusic(GetRandomClip(m_musicClips));
+    }
+
+    private AudioClip GetRandomClip(AudioClip[] clips)
+    {
+        AudioClip randomClip = clips[Random.Range(0, clips.Length)];
+        return randomClip;
+    }
+
+    private void PlayBackgroundMusic(AudioClip musicClip)
+    {
+        if (!m_musicEnabled || !musicClip || !m_musicSource)
+        {
+            return;
+        }
+        
+        m_musicSource.Stop();
+        m_musicSource.clip = musicClip;
+        m_musicSource.volume = m_musicVolume;
+        m_musicSource.loop = true;
+        m_musicSource.Play();
+    }
+
+    private void UpdateMusic()
+    {
+        if (m_musicSource.isPlaying != m_musicEnabled)
+        {
+            if (m_musicEnabled)
+            {
+                PlayBackgroundMusic(GetRandomClip(m_musicClips));
+            }
+            else
+            {
+                m_musicSource.Stop();
+            }
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        m_musicEnabled = !m_musicEnabled;
+        UpdateMusic();
+    }
 }
