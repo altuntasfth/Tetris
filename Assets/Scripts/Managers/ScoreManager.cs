d95bc87c1d61b2b624d49115bf53f57fc3156a46
Fatih <fatihaltuntas44@gmail.com>
2020-12-22 09:51:18 +0300

    added some UI to show off score.

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class ScoreManager : MonoBehaviour
     private int m_score = 0;
     private int m_lines;
     private int m_level = 1;
+
+    public Text m_linesText;
+    public Text m_levelText;
+    public Text m_scoreText;
 
     private const int m_minLines = 1;
     private const int m_maxLines = 4;
@@ -38,11 +43,43 @@ public class ScoreManager : MonoBehaviour
                 m_score += 1200 * m_level;
                 break;
         }
+        
+        UpdateUIText();
     }
 
     public void Reset()
     {
         m_level = 1;
         m_lines = m_linesPerLevel * m_level;
+    }
+
+    private void UpdateUIText()
+    {
+        if (m_linesText)
+        {
+            m_linesText.text = m_lines.ToString();
+        }
+        
+        if (m_levelText)
+        {
+            m_levelText.text = m_level.ToString();
+        }
+        
+        if (m_scoreText)
+        {
+            m_scoreText.text = PadZero(m_score, 5);
+        }
+    }
+
+    private string PadZero(int n, int padDigits)
+    {
+        string nStr = n.ToString();
+
+        while (nStr.Length < padDigits)
+        {
+            nStr = "0" + nStr;
+        }
+
+        return nStr;
     }
 }
